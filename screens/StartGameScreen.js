ab4ee3170044c682b3f8aa0abda01af98dc289e9
Brian Artea <brian@expertdesign.cc>
2020-07-28 20:44:35 -0400

    tyling a View as a Card Container (with Drop Shadows & Rounded Corners)

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game!</Text>
+      <Text style={styles.title}>Start New Game!</Text>
       <View style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
@@ -25,14 +25,23 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   title: {
-    fontSize: 30,
+    fontSize: 27,
     marginVertical: 10
 
   },
   inputContainer: {
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center'
+    marginTop: 20,
+    alignItems: 'center',
+    shadowColor: 'black',
+    //textShadowOffset: { width: 2, height: 2 },
+    shadowRadius: 10,
+    shadowOpacity: 0.25,
+    elevation: 8,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20
   },
   buttonContainer: {
     flexDirection: 'row',
